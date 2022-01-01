37fccc59965818aa4709e0901c6e04775bbee47b
Nave <68282476+ThatGuyVanquish@users.noreply.github.com>
2022-01-01 16:48:40 +0200

    User class
    Added relevant methods and fields

--- Server/src/main/java/bgu/spl/net/User.java
+++ Server/src/main/java/bgu/spl/net/User.java
@@ -1,5 +1,6 @@
 package bgu.spl.net;
 
+import java.sql.Time;
 import java.util.Vector;
 
 public class User {
@@ -7,18 +8,24 @@ public class User {
     private int userId;
     private String username;
     private String password;
+    private String birthday;
     private Vector<User> followingList;
     private Vector<User> followersList;
+    private Vector<User> blockList;
     private boolean loginStatus;
+    private int numOfPosts;
     
-    public User(int userId, String username, String password) {
+    public User(int userId, String username, String password, String birthday) {
         this.userId = userId;
         this.username = username;
         this.password = password;
+        this.birthday = birthday;
         this.followingList = new Vector<>();
         this.followersList = new Vector<>();
+        this.blockList = new Vector<>();
         this.loginStatus = false;
-    }
+        this.numOfPosts = 0;
+    }   
 
     public void login(String username, String password) {
         if (username.equals(this.username) && password.equals(this.password)) loginStatus = true;
@@ -46,7 +53,6 @@ public class User {
         else {
             // send error message of unsuccessfull follow
         }
-        
     }
 
     /**
@@ -58,14 +64,14 @@ public class User {
      * ^ meaning a user should exist before calling this
      *
      */
-    public void unfollow(User username) {
+    public void unfollow(User username, boolean blockReq) {
         if (this.followersList.indexOf(username) != -1) {
             this.followingList.remove(username);
             username.unfollowedBy(this);
-            // send ack message of successfull unfollow
+            // send ack message of successfull unfollow if not blockReq
         }
         else {
-            // send error message of unsuccessfull unfollow
+            // send error message of unsuccessfull unfollow if not blockReq
         }
     }
 
@@ -75,6 +81,31 @@ public class User {
 
     public void unfollowedBy(User username) {
         this.followersList.remove(username);
+    }
+
+    public void block(User username) {
+        this.unfollow(username, true);
+        username.unfollow(this, true);
+        this.blockList.add(username);
+        // send ack message to acknowledge successfull block
+    }
+
+    public double getAge() {
+        int day = Integer.parseInt(this.birthday.substring(0,2));
+        int month = Integer.parseInt(this.birthday.substring(3,5));
+        int year = Integer.parseInt(this.birthday.substring(6));
+        long millis = System.currentTimeMillis();  
+        java.sql.Date date = new java.sql.Date(millis);
+        double age = date.getYear() - year - (date.getMonth() - month)/12 - (date.getDay() - day)/365;
+        return age;
+    }
+
+    public void post() { // Don't know if this should be more elaborate
+        this.numOfPosts++;
+    }
+
+    public String getStats() {
+        return this.getAge() + " " + this.numOfPosts + " " + this.followersList.size() + " " + this.followingList.size();
     }
 
 }
