1602136c4a825afaca9526247134b11ee2aa8c80
Nave <68282476+ThatGuyVanquish@users.noreply.github.com>
2022-01-02 12:00:24 +0200

    Database class
    Created a class to hold maps of relevant data:
    Connection IDs to Connection Handlers,
    Usernames (String) to User objects

--- Server/src/main/java/bgu/spl/net/Database.java
+++ Server/src/main/java/bgu/spl/net/Database.java
@@ -1,5 +1,33 @@
 package bgu.spl.net;
 
+import java.util.concurrent.ConcurrentHashMap;
+
+import bgu.spl.net.srv.ConnectionHandler;
+
 public class Database {
-    
+    private ConcurrentHashMap<Integer, ConnectionHandler> cHandlerMap;
+    private ConcurrentHashMap<ConnectionHandler, User> userMap; // Maybe irrelevant as the BidiMessagingProtocol has a user field
+    private ConcurrentHashMap<String, User> registeredUsers;
+
+    private static class singletonHolder {
+        private static final Database instance = new Database();
+    }
+
+    public Database getInstance() {
+        return singletonHolder.instance;
+    }
+
+    private Database() {
+        this.cHandlerMap = new ConcurrentHashMap<>();
+        this.userMap = new ConcurrentHashMap<>();
+        this.registeredUsers = new ConcurrentHashMap<>();
+    }
+
+    public void register(String username, User user) {
+        this.registeredUsers.put(username, user);
+    }
+
+    public boolean isRegistered(String username) {
+        return (registeredUsers.get(username) != null);
+    }
 }
