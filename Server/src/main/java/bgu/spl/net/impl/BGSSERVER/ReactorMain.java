5e755412b8764f27b26bcbaaa9a57828b832a98d
Nave <68282476+ThatGuyVanquish@users.noreply.github.com>
2022-01-02 22:21:19 +0200

    TPC and Reactor mains

--- Server/src/main/java/bgu/spl/net/impl/BGSSERVER/ReactorMain.java
+++ Server/src/main/java/bgu/spl/net/impl/BGSSERVER/ReactorMain.java
@@ -1,5 +1,15 @@
 package bgu.spl.net.impl.BGSSERVER;
 
+import bgu.spl.net.api.EncDecImpl;
+import bgu.spl.net.api.BiDi.BidiMessagingProtocolImpl;
+import bgu.spl.net.srv.Server;
+
 public class ReactorMain {
-    
+    public static void main(String[] args) {
+        Server<String> server = Server.reactor(Integer.parseInt(args[1]), 
+        Integer.parseInt(args[0]),
+        () -> new BidiMessagingProtocolImpl(),
+        () -> new EncDecImpl());
+        server.serve();
+    }
 }
--- Server/src/main/java/bgu/spl/net/impl/BGSSERVER/TPCMain.java
+++ Server/src/main/java/bgu/spl/net/impl/BGSSERVER/TPCMain.java
@@ -1,5 +1,14 @@
 package bgu.spl.net.impl.BGSSERVER;
 
+import bgu.spl.net.api.EncDecImpl;
+import bgu.spl.net.api.BiDi.BidiMessagingProtocolImpl;
+import bgu.spl.net.srv.Server;
+
 public class TPCMain {
-    
+    public static void main(String[] args) {
+        Server<String> server = Server.threadPerClient(Integer.parseInt(args[0]), 
+        () -> new BidiMessagingProtocolImpl(),
+        () -> new EncDecImpl());
+        server.serve();
+    }
 }
